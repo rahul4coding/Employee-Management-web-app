1526d383d26ba90b08fa1c65d4875d77352770e6
rkbrahul <rahulbhatija123@gmail.com>
2018-09-25 03:05:04 +0530

    updated

--- server/login/login.js
+++ server/login/login.js
@@ -41,9 +41,6 @@ router.post('/', function (req, res) {
   });
 
 
-  module.exports = router;
-
-
-
-
 })
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyparser = require("body-parser");
 
-var app = exxpress();
+var app = express();
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
