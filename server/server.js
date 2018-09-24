d52c16dd5c6964f4113da719a0a3e97fb80582b8
rkbrahul <rahulbhatija123@gmail.com>
2018-09-25 02:50:47 +0530

    updated

---
+++ server/server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var bodyparser = require("body-parser");
+
+var app = exxpress();
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyparser.json());
+
+app.use(bodyparser.urlencoded({
+  extended: false
+}));
+
+var login = require('./login/login');
+
+app.use("/login", login);
+
+//assign port number
+
+app.listen(8082);
+console.log("server listening to port no 8082");
